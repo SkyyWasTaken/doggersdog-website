0e0ce820dc26eaae47325b99ab883547d116cf09
SkyyWasTaken <doggersdog@proton.me>
2024-12-26 20:27:39 -0500

    Added Github secret to Pipeline stack

--- packages/cdk/lib/pipeline-stack.ts
+++ packages/cdk/lib/pipeline-stack.ts
@@ -1,12 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import {Secret} from "aws-cdk-lib/aws-secretsmanager";
 
 export class DoggersDogPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const pipeline = new CodePipeline(this, 'Pipeline', {
+        new Secret(this, 'GithubToken', {
+            secretName: 'github-token'
+        })
+
+        new CodePipeline(this, 'Pipeline', {
             pipelineName: 'DoggersDogPipeline',
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.gitHub('SkyyWasTaken/doggersdog-website', 'main'),
